82a252105497da665886b61017adb14adbea2142
Niles Rogoff <me@niles.xyz>
2019-02-27 21:16:09 -0500

    Count number of yields between cr_aio_read successes

--- test.c
+++ test.c
@@ -6,11 +6,20 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+int i = 0;
+
 void* thread_func(cr_env* env, int tid, void* baton) {
    if (tid == 1) {
+      int bufs[100];
       while (!env->frames[0].dead) {
-	 printf("thread 0 yielded\n");
+	 if (i < 100) {
+	    bufs[i]++;
+	 }
+	 //printf("thread 0 yielded\n");
 	 cr_yield(env);
+      }
+      for (int ii = 0; ii < i; ii++) {
+	 printf("yielded %d times\n", bufs[ii]);
       }
       printf("thread 0 died\n");
       return NULL;
@@ -21,7 +30,8 @@ void* thread_func(cr_env* env, int tid, void* baton) {
    int read;
    int fd = open("/etc/passwd", O_RDONLY);
    while ((read = cr_aio_buf_read(buf, fd)) > 0) {
-      printf("read %d characters into %p\n", read, buf->buffer);
+      //printf("read %d characters into %p\n", read, buf->buffer);
+      i++;
    }
 
    return NULL;
